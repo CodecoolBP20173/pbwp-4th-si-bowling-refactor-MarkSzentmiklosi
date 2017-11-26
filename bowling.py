c3b901eb36fa016c64155fa85dfefdde37ea1a9b
Mark Szentmiklosi <szentmiklosi.mark@gmail.com>
2017-11-26 17:47:52 +0100

    merged conditions

--- bowling.py
+++ bowling.py
@@ -2,35 +2,33 @@ def score(game):
     """ 
     Calculates the total score of a bowling game.
     """
-    
-    result = 0
+    score = 0
     frame = 1
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
             last = get_value(game[i-1])
-            result += 10 - last
+            score += 10 - last
         else:
-            result += get_value(game[i])
+            score += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                score += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+                score += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    score += 10 - get_value(game[i+1])
                 else:
-                    result += get_value(game[i+2])
+                    score += get_value(game[i+2])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
             in_first_half = True
+        else:
+            in_first_half = False
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
-    return result
+    return score
 
 
 def get_value(char):
