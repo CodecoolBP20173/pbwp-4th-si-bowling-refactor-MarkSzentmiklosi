e3e52986c7394f26ce3481255eaa80ba2eb86a01
Mark Szentmiklosi <szentmiklosi.mark@gmail.com>
2017-11-26 18:09:13 +0100

    Deleted last var, because it was only used once

--- bowling.py
+++ bowling.py
@@ -7,38 +7,34 @@ def score(game):
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            last = get_value(game[i-1])
-            score += 10 - last
+            score += 10 - get_value(game[i - 1])
         else:
             score += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                score += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                score += get_value(game[i+1])
-                if game[i+2] == '/':
-                    score += 10 - get_value(game[i+1])
+                score += get_value(game[i + 1])
+            elif game[i] in "Xx":
+                score += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    score += 10 - get_value(game[i + 1])
                 else:
-                    score += get_value(game[i+2])
-        if not in_first_half:
+                    score += get_value(game[i + 2])
+        if not in_first_half or game[i] in "Xx":
             frame += 1
             in_first_half = True
         else:
             in_first_half = False
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
+
     return score
 
 
 def get_value(char):
     """
-    Returns the value of the actual roll
+    Returns the point of a roll
     """
-
     if '1' <= char <= '9':
         return int(char)
-    elif char in ["x", "X", "/"]:
+    elif char in "Xx/":
         return 10
     elif char == '-':
         return 0
